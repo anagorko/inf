9dadbfcc43f42dcdc67d1c92ae58735b13ad70c2
Virtover <comodore9636@gmail.com>
2020-09-11 13:17:57 +0200

    Update modpow.py

--- home/kolszak/modpow.py
+++ home/kolszak/modpow.py
@@ -3,12 +3,12 @@ Recursive (a ** b) % m.
 """
 
 
-def power(a: int, p: int, mod: int) -> int:
+def power(a: int, p: int, m: int) -> int:
     """Compute (a ** p) % m."""
     if p == 0:
         return 1
     if p % 2 == 1:
-        return (a * pow(a * a, p // 2, mod)) % mod
+        return (a * pow(a * a, p // 2, m)) % m
     if p % 2 == 0:
-        return pow(a * a, p // 2, mod) % mod
+        return pow(a * a, p // 2, m) % m
 
