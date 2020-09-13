bc0682d40fb4b267c13f03ed3e0f93abb370fb6d
anagorko <amn@mimuw.edu.pl>
2020-09-13 17:36:05 +0200

    Update change.py

--- greedy_change/change.py
+++ greedy_change/change.py
@@ -9,6 +9,6 @@ from typing import Dict
 COINS = [1, 2, 5, 10, 20, 50, 100, 200]
 
 
-def change(amount: int) -> Dict[int]:
+def change(amount: int) -> Dict[int, int]:
     """Find how many coins of each denomination we need to get given amount.
     The total number of coins should be minimal."""
