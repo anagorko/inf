7636cca5cf44cab70d695bd5353f251bd8e86825
ArnoldLowefel <71127477+ArnoldLowefel@users.noreply.github.com>
2020-09-13 17:17:52 +0200

    Update number_representation.py

--- home/nagorko/number_representation.py
+++ home/nagorko/number_representation.py
@@ -4,8 +4,8 @@ Number system conversions: dec, hex and bin bases.
 
 import unittest
 
-string2num = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
-              1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+string2num = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "0": 0,
+              1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 0: "0",
               "A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15,
               10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F"
               }
@@ -14,7 +14,9 @@ string2num = {"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9
 def int2dec(n: int) -> str:
     """Convert an integer to a string in decimal number system."""
     result = ""
-    while n % 10 is not 0:
+    if n == 0:
+        result = "0"
+    while n is not 0:
         result = string2num[n % 10] + result
         n = n // 10
     return result
@@ -23,7 +25,9 @@ def int2dec(n: int) -> str:
 def int2bin(n: int) -> str:
     """Convert an integer to a string in binary number system."""
     result = ""
-    while n % 2 is not 0:
+    if n == 0:
+        result = "0"
+    while n is not 0:
         result = string2num[n % 2] + result
         n = n // 2
     return result
@@ -33,7 +37,9 @@ def int2hex(n: int) -> str:
     """Convert an integer to a string in hexadecimal number system."""
     divisor = 16
     result = ""
-    while n % divisor is not 0:
+    if n == 0:
+        result = "0"
+    while n is not 0:
         result = string2num[n % divisor] + result
         n = n // divisor
     return result
@@ -77,13 +83,11 @@ class NumberRepresentationTest(unittest.TestCase):
 
     def test_handcrafted_examples(self):
         """Test some small hand picked examples"""
-        self.assertEqual(hex2int("F"), 15)
-        self.assertEqual(int2dec(1123), "1123")
-        self.assertEqual(int2bin(15), "1111")
-        self.assertEqual(int2hex(234), "EA")
-        self.assertEqual(bin2int("111111"), 63)
-        self.assertEqual(dec2int("31415"), 31415)
+        for i in range(1000):
+            self.assertEqual(int2dec(i), str(i))
+            self.assertEqual(dec2int(str(i)), i)
 
 
 if __name__ == '__main__':
-    print(int2hex(234))
+    print(dec2int("2"))
+    print(int2hex(0))
