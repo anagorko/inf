2a0e675b3444f838887e181bf85c4754a76b3ab9
Virtover <comodore9636@gmail.com>
2020-09-15 13:48:36 +0200

    Create primality.py

---
+++ home/kolszak/primality.py
@@ -0,0 +1,25 @@
+"""
+Check if given number is prime.
+"""
+
+import unittest
+import math
+
+
+def is_prime(n: int) -> bool:
+    """Check id given number is prime."""
+    for i in range(2, int(math.sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
+
+
+class IsPrimeTest(unittest.TestCase):
+    """is_prime function tests."""
+
+    def is_prime_test(self):
+        """Test some small hand picked example."""
+        self.assertEqual(is_prime(78), False)
+        self.assertEqual(is_prime(7), True)
+        self.assertEqual(is_prime(21), False)
+
