a9b903786510b4f66c6b4a7c403e00480b9cff00
Virtover <comodore9636@gmail.com>
2020-09-15 13:11:09 +0200

    Update modpow.py

--- home/kolszak/modpow.py
+++ home/kolszak/modpow.py
@@ -2,6 +2,8 @@
 Recursive (a ** b) % m.
 """
 
+import unittest
+
 
 def power(a: int, p: int, m: int) -> int:
     """Compute (a ** p) % m."""
@@ -11,4 +13,15 @@ def power(a: int, p: int, m: int) -> int:
         return (a * pow(a * a, p // 2, m)) % m
     if p % 2 == 0:
         return pow(a * a, p // 2, m) % m
+
+
+class ModPowerUnitTest(unittest.TestCase):
+    """Unit tests for power function."""
+
+    def test_mod_power(self):
+        """Test some small hand picked example."""
+        for i in range(10):
+            for j in range(10):
+                for m in range(10):
+                    self.assertEqual(power(i, j, m), (i ** j) % m)
 
