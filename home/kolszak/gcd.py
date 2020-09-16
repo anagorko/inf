47bcaa396ca24677a3d457fdf722a9a3f1340851
Virtover <comodore9636@gmail.com>
2020-09-16 14:22:39 +0200

    Update gcd.py

--- home/kolszak/gcd.py
+++ home/kolszak/gcd.py
@@ -25,14 +25,14 @@ def gcd_iterative(a: int, b: int) -> int:
 class GcdTest(unittest.TestCase):
     """Gcd compute functions tests."""
 
-    def gcd_recursive_test(self):
+    def test_gcd_recursive_test(self):
         """Test some small hand picked example."""
         self.assertEqual(gcd_recursive(48, 18), 6)
         self.assertEqual(gcd_recursive(123, 369), 123)
         self.assertEqual(gcd_recursive(8, 12), 4)
         self.assertEqual(gcd_recursive(12, 8), 4)
 
-    def gcd_iterative_test(self):
+    def test_gcd_iterative_test(self):
         """Test some small hand picked example."""
         self.assertEqual(gcd_iterative(48, 18), 6)
         self.assertEqual(gcd_iterative(123, 369), 123)
