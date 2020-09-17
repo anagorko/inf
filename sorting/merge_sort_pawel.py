189c4126081009d3e9391ff1e67b8f8ab3d9b9d5
ArnoldLowefel <71127477+ArnoldLowefel@users.noreply.github.com>
2020-09-17 17:55:24 +0200

    Update merge_sort_pawel.py

--- sorting/merge_sort_pawel.py
+++ sorting/merge_sort_pawel.py
@@ -3,6 +3,7 @@ Merge sort implementation.
 """
 
 import unittest
+import timeit
 
 
 def merge(first_list, second_list):
@@ -24,7 +25,7 @@ def merge(first_list, second_list):
 
 
 def merge_sort(list2sort):
-    if len(list2sort) == 1:
+    if len(list2sort) <= 1:
         return list2sort
     return merge(merge_sort(list2sort[0:len(list2sort) // 2]), merge_sort(list2sort[len(list2sort) // 2:]))
 
@@ -38,6 +39,17 @@ class NumberRepresentationTest(unittest.TestCase):
         self.assertEqual(merge_sort([1, 2, 4, 5, 8, 2, 3, 6, 7]), [1, 2, 2, 3, 4, 5, 6, 7, 8])
 
 
+def benchmark():
+    """Recursive vs iterative benchmark."""
+    for n in range(1000):
+        time_sorting = timeit.timeit(f'merge_sort.merge_sort(l)',
+                    setup=f'import merge_sort\nimport random\n'f'l=[random.randint(1,1000) for _ in range({100*n})]',
+                    number=1)
+
+        print(f'n = {n}, iterative time: {time_sorting:.8f},')
+
+
 if __name__ == '__main__':
+    benchmark()
     print(merge_sort([2, 1, 45, 567, 2137, 543, 69, 420, 27, 21, 13, 1, 3, 5, 9, 17, 19, 21718281828, 31415, 16115,
                       3245634]))
