ead032acc532db56500aa24d177f04d081046ac3
Andrzej Nagorko <amn@mimuw.edu.pl>
2020-10-23 12:19:28 +0200

    Specyfikacja zadania obrazki

---
+++ zbior_zadan_cke/64/obrazki.py
@@ -0,0 +1,37 @@
+"""
+Solution of problem 64 "Obrazki".
+"""
+
+from typing import List, IO
+
+
+Image = List[List[int]]
+
+
+def read_image(file: IO) -> Image:
+    """Reads one image from given file."""
+
+
+def strip(image: Image) -> Image:
+    """Remove parity bits from the image."""
+
+
+def count_dark_bits(image: Image):
+    """Counts number of ones in the stripped image."""
+
+
+def get_row(image: Image, n: int) -> List[int]:
+    """Get nth row."""
+
+
+def get_column(image: Image, n: int) -> List[int]:
+    """Get nth column."""
+
+
+def is_square(sequence: List[int]) -> bool:
+    """Check whether sequence is a square."""
+
+
+def count_parity(sequence: List[int]) -> int:
+    """1 iff the number ones in the sequence is odd."""
+
